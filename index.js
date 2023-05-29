08dc54337cc7d13334e817f660bd4fff91126a7a
Riley Tomasek <riley.tomasek@gmail.com>
2023-05-29 19:27:32 -0400

    Remove Remix specific rules/config
    I'll use the Remix version directly for Remix packages so they aren't
    needed here.

--- index.js
+++ index.js
@@ -77,20 +77,6 @@ const config = {
         ...typescriptRules,
       },
     },
-    {
-      files: [
-        '**/routes/**/*.js?(x)',
-        '**/routes/**/*.tsx',
-        'app/root.js?(x)',
-        'app/root.tsx',
-      ],
-      rules: {
-        // Routes may use default exports without a name. At the route level
-        // identifying components for debugging purposes is less of an issue, as
-        // the route boundary is more easily identifiable.
-        'react/display-name': OFF,
-      },
-    },
   ],
 };
 
